b875dbed96f38bddb07ec05cee269111777cc486
Jesse Lew <lewj85@gmail.com>
2017-09-11 00:39:43 -0400

    Added other voices to matrixToLily

--- matrixToLily.py
+++ matrixToLily.py
@@ -6,64 +6,71 @@ def matrixToLily(key, major, finalMTX, measures, maxVoices):
     #       7th chord, tonality, inversion, prev chord root, pickup, beat, measure
     finalString = ""
     j = 0
-    voice = 0
+    #voice = 0
     totalNotes = finalMTX.shape[0]
     #print('total notes = ', totalNotes)
 
     for i in range(1, measures + 1):  # while there's another measure in the matrix
+
         # TO DO: all voices need to be added...
-        #for voice in range(maxVoices):
+        for voice in range(maxVoices - 1, -1, -1):
+            prevj = j  # store old j value to reset for each voice
 
-        # while you haven't reached the end of the piece yet AND you're still on the current measure
-        # NOTE: the statement below only works because of the short-circuit "and"!!!
-        while j < totalNotes and finalMTX[j][11][voice] == i:
-            print('loop #', i)
-            if key == 'C':
-                if major == 1:
-                    # pitch
-                    finalString += str.lower(ntp.numToPitch(finalMTX[j][0][voice], key))
-                    #print(finalString)
+            # while you haven't reached the end of the piece yet AND you're still on the current measure
+            # NOTE: the statement below only works because of the short-circuit "and"!!!
+            while j < totalNotes and finalMTX[j][11][voice] == i:
+                #print('loop #', i)
+                if key == 'C':
+                    if major == 1:
+                        # pitch
+                        finalString += str.lower(ntp.numToPitch(finalMTX[j][0][voice], key))
+                        #print(finalString)
 
-                    # duration optional for repeated durations, but we will be using it
-                    # NOTE: duration in matrix does not equal LilyPond duration notation!!!
-                    #   so must use dictionary to return correct symbol
-                    # TO DO: fix this to take timesig param and calculate/convert durations properly
-                    matrixDuration = str(finalMTX[j][1][voice])
-                    lilyDuration = {
-                        '1': '4',
-                        '2': '2',
-                        '3': '2.',  # for 3/4 time
-                        '4': '1'
-                    }.get(matrixDuration, '1')  # find 'pitch' in dictionary above, otherwise default to 1
-                    finalString += lilyDuration
-                    #print(finalString)
+                        # duration optional for repeated durations, but we will be using it
+                        # NOTE: duration in matrix does not equal LilyPond duration notation!!!
+                        #   so must use dictionary to return correct symbol
+                        # TO DO: fix this to take timesig param and calculate/convert durations properly
+                        matrixDuration = str(finalMTX[j][1][voice])
+                        lilyDuration = {
+                            '1': '4',
+                            '2': '2',
+                            '3': '2.',  # for 3/4 time
+                            '4': '1'
+                        }.get(matrixDuration, '1')  # find 'pitch' in dictionary above, otherwise default to 1
+                        finalString += lilyDuration
+                        #print(finalString)
 
-                    # TO DO: direction only needs to check if > 4 for "'" or < -4 for ","
+                        # TO DO: direction only needs to check if > 4 for "'" or < -4 for ","
 
-                    # chord root and 7th chord not needed for LilyPond
+                        # chord root and 7th chord not needed for LilyPond
 
-                    # TO DO: tonality not needed in major (until we add secondary dominants)
+                        # TO DO: tonality not needed in major (until we add secondary dominants)
 
-                    # inversion and prev chord root not needed for LilyPond
+                        # inversion and prev chord root not needed for LilyPond
 
-                    # TO DO: pickup needed for LilyPond but not for chorale
+                        # TO DO: pickup needed for LilyPond but not for chorale
 
-                    # beat not needed for LilyPond
+                        # beat not needed for LilyPond
 
-                    # TO DO: articulation will go here eventually
+                        # TO DO: articulation will go here eventually
 
-                    # put a space after each note
-                    finalString += " "
-                    #print(finalString)
+                        # put a space after each note
+                        finalString += " "
+                        #print(finalString)
 
-                    # TO DO: ties go here with a space after them too
+                        # TO DO: ties go here with a space after them too
 
-            j += 1  # go to next note
+                j += 1  # go to next note
+
+            # measure symbol is optional, but we will be using it
+            finalString += "|\n"
+            # print(finalString)
+
+            # reset j to old value for the next voice, but not after the last voice
+            if voice != 0:
+                j = prevj
 
         i += 1  # go to next measure
-
-        # measure symbol is optional, but we will be using it
-        finalString += "|\n"
-        print(finalString)
+        finalString += "\n"
 
     return finalString
